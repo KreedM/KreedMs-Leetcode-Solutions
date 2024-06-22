12082670415a78cdf796eb655f5e151ffce9aee6
KreedM <blackknight3377@gmail.com>
2024-06-21 21:11:10 -0700

    191

---
+++ 191 Number of 1 bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+
+        while(n) {
+            if(n & 1)
+                ++count;
+            
+            n = n >> 1;
+        }
+
+        return count;
+    }
+};
