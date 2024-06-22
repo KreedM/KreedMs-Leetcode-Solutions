13db3cb174f37f2a11ec378b9bd6e3bb47757793
KreedM <blackknight3377@gmail.com>
2024-06-21 21:17:15 -0700

    Better 191

--- 191 Number of 1 bits.cpp
+++ 191 Number of 1 bits.cpp
@@ -4,10 +4,8 @@ public:
         int count = 0;
 
         while(n) {
-            if(n & 1)
-                ++count;
-            
-            n = n >> 1;
+            ++count;
+            n &= n - 1;
         }
 
         return count;
